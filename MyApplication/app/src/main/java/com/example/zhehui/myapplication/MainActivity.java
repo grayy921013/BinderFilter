5c352b8a5726dc32cb05d251fdcb4819a8408ca8
Zhehui Zhou <grayy921013@gmail.com>
2017-10-30 12:38:30 -0700

    Add an application for receiving intent

--- MyApplication/app/src/main/java/com/example/zhehui/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/zhehui/myapplication/MainActivity.java
@@ -3,15 +3,25 @@ package com.example.zhehui.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sendBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent sendIntent = new Intent(this, Main2Activity.class);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "4122519664.");
-        startActivity(sendIntent);
+        sendBtn = (Button)findViewById(R.id.send_btn);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent("com.example.zhehui.receiveapplication.MainActivity");
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "4122519664.");
+                startActivity(sendIntent);
+            }
+        });
     }
 }
